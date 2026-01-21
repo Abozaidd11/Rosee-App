172125a36aa3771804e8e57296e09806b624e16a
ahmed gomaa <ahmed.gomaa321@gmail.com>
2026-01-21 17:11:39 +0200

    fix(feat/notifications): remove static token and update endpoint

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -1,33 +1,31 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  //   const token = await getToken({
-  //   req,
-  //   secret: process.env.NEXTAUTH_SECRET,
-  //   secureCookie: process.env.NODE_ENV === "production",
-  // });
+// export async function GET(req: NextRequest) {
+//   const token = await getToken({
+//   req,
+//   secret: process.env.NEXTAUTH_SECRET,
+//   secureCookie: process.env.NODE_ENV === "production",
+// });
 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjdjMDYzOWQ4MzZlZThiZTcwNjE5MGE5Iiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzY5MDA3MDA2fQ.nNAkIB4AXgsa9xnsUUR58yf1wslKmt31IA5jyfa3Pis";
-  if (!token) {
-    return new Response("Unauthorized", {
-      status: 401,
-      statusText: "Unauthorized",
-    });
-  }
+//   if (!token) {
+//     return new Response("Unauthorized", {
+//       status: 401,
+//       statusText: "Unauthorized",
+//     });
+//   }
 
-  const { searchParams } = new URL(req.url);
-  const page = searchParams.get("page") || "1";
-  const limit = searchParams.get("limit") || "10";
+//   const { searchParams } = new URL(req.url);
+//   const page = searchParams.get("page") || "1";
+//   const limit = searchParams.get("limit") || "10";
 
-  const res = await fetch(process.env.API + `/notifications/admin?page=${page}&limit=${limit}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
+//   const res = await fetch(process.env.API + `/notifications/user?page=${page}&limit=${limit}`, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//   });
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  return NextResponse.json(data, { status: res.status });
-}
+//   return NextResponse.json(data, { status: res.status });
+// }
