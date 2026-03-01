bad80c7f707e8d06e5c5950e57f5ab6e0d21f5d9
Maged Mohamed <magdahmed624@gmail.com>
2026-03-02 01:58:50 +0200

    refactor(i18n): update ProductCard and ProductsList components to use async translations and locale-based styling

--- src/components/shared/products-list.tsx
+++ src/components/shared/products-list.tsx
@@ -4,7 +4,7 @@ import { Rose } from "lucide-react";
 import { cn } from "@/lib/utils/tailwind-merge";
 import { getProducts } from "@/lib/services/product.service";
 import { SearchParams } from "@/lib/types/global";
-import { getTranslations } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
 
 type ProductsListProps = {
   searchParams?: SearchParams;
@@ -15,6 +15,9 @@ const ProductsList = React.forwardRef<HTMLDivElement, ProductsListProps>(
   async ({ searchParams, className }, ref) => {
     // Translations
     const t = await getTranslations("product-listing");
+
+    // Hooks
+    const locale = await getLocale();
 
     // Services
     const payload = await getProducts(searchParams);
@@ -28,7 +31,12 @@ const ProductsList = React.forwardRef<HTMLDivElement, ProductsListProps>(
 
         {/* No data to display.  */}
         {!payload.products.length && (
-          <div className="flex flex-col justify-center items-center gap-3 col-span-4 py-20 font-medium text-zinc-500 text-sm capitalize leading-none">
+          <div
+            className={cn(
+              locale === "ar" && "font-tajawal",
+              "flex flex-col justify-center items-center gap-3 col-span-4 py-20 font-medium text-zinc-500 text-sm capitalize leading-none"
+            )}
+          >
             <Rose className="size-12 text-zinc-500" strokeWidth={1.75} />
             {t("no-products-message")}
           </div>
